382211be1da41578688961227859bc617ac341bd
lucy090110 <799335607@qq.com>
2020-05-20 22:38:54 +0800

    week06

---
+++ week06/matchString.js
@@ -0,0 +1,62 @@
+//查找abababx
+function match(string){
+    let state = start;
+    for(let i of string){
+        state = state(i);
+    }
+    return state === end;
+}
+function start(c){
+    if(c=='a'){
+        return foundA;
+    }else{
+        return start;
+    }
+}
+
+function end(c) {
+    return end;
+}
+function foundA(c){
+    if(c === 'b'){
+        return foundB;
+    }else{
+        return start(c);
+    }
+}
+function foundB(c){
+    if(c === 'a'){
+        return foundA2;
+    }else{
+        return start(c);
+    }
+}
+function foundA2(c){
+    if(c === 'b'){
+        return foundB2;
+    }else{
+        return start(c);
+    }
+}
+function foundB2(c){
+    if(c === 'a'){
+        return foundA3;
+    }else{
+        return start(c);
+    }
+
+}
+function foundA3(c){
+    if(c === 'b'){
+        return foundB3;
+    } else{
+        return start(c);
+    }
+}
+function foundB3(c){
+    if(c === 'x'){
+        return end;
+    } else{
+        return foundB2(c);
+    }
+}
